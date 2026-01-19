23623212e75d450cb19bb15cba22b2e0db63139f
alfha-x-md <alfhaxmd@gmail.com>
2026-01-19 08:41:01 +0530

    Update confing.js

--- confing.js
+++ confing.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "5McUyI4D#NwF3TfNY78K0Yi-8rGD7EVES0Pzw7rSgKH7pNS40fd8",
+SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://res.cloudinary.com/dhpfn4umb/image/upload/v1768786473/ej7p8gkhfezafeyuhjpl.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*HELLO ALFHA X MD IS ALIVE NOW*",
 BOT_OWNER: '94784167385',  // Replace with the owner's phone number
